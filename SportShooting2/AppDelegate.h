f66e98856fafc8036afb28ab0c7fa08d7ebf397b
Othman Sbai <sbai.oth@gmail.com>
2016-06-04 22:53:20 -0700

    Initial Commit

---
+++ SportShooting2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SportShooting2
+//
+//  Created by Othman Sbai on 6/4/16.
+//  Copyright © 2016 Othman Sbai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SportShooting2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SportShooting2
+//
+//  Created by Othman Sbai on 6/4/16.
+//  Copyright © 2016 Othman Sbai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
